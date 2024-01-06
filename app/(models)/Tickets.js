78a30294eda3851e0cf4c3e5d496fc8571fe7f2b
ankit2153 <ankitkumarpandey2153@gmail.com>
2024-01-07 01:38:48 +0530

    some changes

--- app/(models)/Tickets.js
+++ app/(models)/Tickets.js
@@ -1,7 +1,7 @@
 import mongoose,{Schema} from "mongoose";
 
 
-mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI);
+mongoose.connect(process.env.NEXT_PUBLIC_MONGOURI);
 
 
 mongoose.Promise = global.Promise;
