fba0e675f6ba90da778e64f2a249165e13d4d292
ankit2153 <ankitkumarpandey2153@gmail.com>
2024-01-07 13:01:45 +0530

    ticket changes-2

--- app/page.jsx
+++ app/page.jsx
@@ -1,59 +1,49 @@
-
 import TicketCard from "./(components)/TicketCard";
- 
 
-  const getTickets = async () => {
-    try {
-      const res = await fetch("http://localhost:3000/api/Tickets", {
+const getTickets = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/Tickets`, {
+      cache: "no-store",
+    });
 
-        cache: "no-store",
+    return res.json();
+  } catch (error) {
+    console.log("Failed to get Tickets", error);
+  }
+};
 
-      });
+const Home = async () => {
+  const { tickets } = (await getTickets()) || { tickets: [] };
 
-      return res.json();
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
 
-    } catch (error) {
+  return (
+    <div className="p-4">
+      Dashboard
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4 ">
+              <h2 className="text-2xl font-extrabold text-green-500 underline">
+                {uniqueCategory}
+              </h2>
 
-      console.log("Failed to get Tickets", error);
-      
-    }
-  };
-
-  const Home = async () => {
-
-
-    const { tickets } = await getTickets() || {tickets:[]};
-
-    const uniqueCategories = [...new Set(tickets?.map(({ category }) => category))];
-
-
-
-    return (
-      <div className="p-4">
-        Dashboard
-        
-        <div>
-          {tickets &&
-            uniqueCategories?.map((uniqueCategory, categoryIndex) => (
-              <div key={categoryIndex} className="mb-4 ">
-                <h2 className="text-2xl font-extrabold text-green-500 underline">
-                  {uniqueCategory}
-                </h2>
-
-                <div className="lg:grid grid-cols-2 xl:grid-cols-4  p-2">
-                  {tickets
-                    .filter((ticket) => ticket.category === uniqueCategory)
-                    .map((filteredTicket, _index) => (
-                      <div className="p-2" key={_index}>
-                        <TicketCard id={_index} ticket={filteredTicket} />
-                      </div>
-                    ))}
-                </div>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4  p-2">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <div className="p-2" key={_index}>
+                      <TicketCard id={_index} ticket={filteredTicket} />
+                    </div>
+                  ))}
               </div>
-            ))}
-        </div>
+            </div>
+          ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Home;
+export default Home;
